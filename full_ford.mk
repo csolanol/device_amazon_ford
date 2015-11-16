457ab0e09109258708f694c73cedb38c732d54be
Carlos Solano <csolano@gmail.com>
2015-11-16 14:12:39 +0100

    Initial commit

---
+++ full_ford.mk
@@ -0,0 +1,16 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/amazon/ford/device.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+
+PRODUCT_CHARACTERISTICS := tablet
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_NAME := full_ford
+PRODUCT_DEVICE := ford
+PRODUCT_BRAND := amazon
+PRODUCT_MODEL := Fire
+PRODUCT_MANUFACTURER := amazon
