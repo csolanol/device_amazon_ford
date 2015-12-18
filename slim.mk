2ae0b6fff7ff0ac1f24189946e07fb6ea9c63ed6
Carlos Solano <csolano@gmail.com>
2015-12-18 10:23:41 +0100

    Move screen resolution to slim.mk

--- slim.mk
+++ slim.mk
@@ -9,9 +9,13 @@ $(call inherit-product, vendor/slim/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration
 $(call inherit-product, device/amazon/ford/full_ford.mk)
 
+TARGET_SCREEN_WIDTH := 600
+TARGET_SCREEN_HEIGHT := 1024
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ford
+#PRODUCT_DEVICE := ford
 PRODUCT_NAME := slim_ford
-PRODUCT_BRAND := amazon
-PRODUCT_MODEL := ford
-PRODUCT_MANUFACTURER := amazon
+PRODUCT_RELEASE_NAME := ford
+#PRODUCT_BRAND := amazon
+#PRODUCT_MODEL := ford
+#PRODUCT_MANUFACTURER := amazon
